c09aece90f7f1caf693f0f7be1d1e6a332fa0b06
Székely Szabolcs Attila (aka RTS) <72965662+szabiszekely@users.noreply.github.com>
2026-06-02 15:33:40 +0200

    2026/06/02 15:25 Stat changes almost done
    I have finished 90% of the creating, testing, stress testing and other nick nags I felt needed to be addressed, I have a running and working prototype of the final result, I just need to implement it into the main frame work... lot of words to only describe that I did most of the stuff needed to add it to the base loop. I will finish it later. But now I will incredibly stressed out. I have a exam tomorrow and I fear I will not gonna pass. I have 4 actually, one is worse then the next. I'm scared and honestly feeling trapped... so I will just go out and clear my mind. I feel like I earned a big stretch after this.
    
    I guess I could let you on what is up with me in this past weeks, but honestly not much. Study, more Study and issues I thought was the end of the world, but now looks like nothing. I thought I will grow out of the fact that I over react on certain things but I guess not. I still pitty myself over it, but there is nothing to do with it. I can have a panic attack, I can try to seek professionals but at the end it is me, and "I" have to figure it out on my own. People don't care about you like that, they can never do that.
    
    We can only try to understand each other or pretend we do, but at the end we all to ourselves. I'm sorry if it's unpleasant to hear. I just don't know anymore who will listen when I cannot afford to.

--- lotth-rpg/Testing_folder/test_run.gd
+++ lotth-rpg/Testing_folder/test_run.gd
@@ -1,7 +1,68 @@
 extends Node
+@export var _test_Stat_boost: Stat_Increases
 
-var Dict: Dictionary = {1:["alma",false],2:["bananna",true],3:["szölö",true]}
+@export var Defense: int ##Every Defense is -1 from a Phisical attack
+@export var Magic_Defense: int ##Every Magic Defense is -1 from Magical attack
+@export var Base_Phisical_Attack: int 
+@export var Base_Magical_Attack: int
+@export var Speed: int ##Every start of a round it will roll a D6 and adds it to the speed, the larger the better
+var Temp_Def = 0
+var Temp_M_Def = 0
+var Temp_BP_A = 0
+var Temp_BM_A = 0
+var Temp_Sped = 0
 
+var header = []
+var database = []
 func _ready() -> void:
-	for i in Dict:
-		print(i," ",Dict[i][0]," ",Dict[i][1])
+	_test_Stat_boost._Get_Current_Stats(header)
+	_Database_append(_test_Stat_boost._Stat_change("Turn",1,{1:["Def",1,false]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",2,{1:["Def",1,true]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",1,{1:["Def",1,true]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",3,{1:["Def",2,true],2:["BP_Attack",2,false]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",1,{1:["Speed",1,true],2:["M_Def",1,true],3:["BP_Attack",3,false]}))
+
+	_Database_append(_test_Stat_boost._Stat_change("Turn",1,{1:["Def",1,true]}))
+	_Database_append(_test_Stat_boost._Stat_change("ROB",1,{1:["BP_Attack",2,true]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",5,{1:["BM_Attack",1,true]}))
+	_Database_append(_test_Stat_boost._Stat_change("Turn",3,{1:["M_Def",2,true],2:["Def",1,false]}))
+	_Database_append(_test_Stat_boost._Stat_change("ROB",0,{1:["Speed",1,true],2:["M_Def",1,true],3:["BP_Attack",1,false]}))
+
+	_Apply_Stats()
+	#print(database, header)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_button"):
+		var temp_remover = []
+		for i in database:
+			i[-1] = i[-1] - 1
+			if i[-1] == 0:
+				temp_remover.append(i)
+		for i in temp_remover:
+			database.erase(i)
+		print(database)
+		_Apply_Stats()
+
+func _Database_append(What_to_add):
+	database.append(What_to_add)
+	header.append(What_to_add[0])
+
+
+func _Apply_Stats():
+	Temp_Def = 0
+	Temp_M_Def = 0
+	Temp_BP_A = 0
+	Temp_BM_A = 0
+	Temp_Sped = 0
+	for i in database:
+		Temp_Def += Defense + i[1][0]
+		Temp_M_Def += Magic_Defense + i[1][1]
+		Temp_BP_A += Base_Phisical_Attack + i[1][2]
+		Temp_BM_A += Base_Magical_Attack + i[1][3]
+		Temp_Sped += Speed + i[1][4]
+	
+	print("DEF ",Temp_Def)
+	print("M_DEF ",Temp_M_Def)
+	print("BASE_PHISICAL_ATTACK ",Temp_BP_A)
+	print("BASE_MAGICAL_ATTACK ",Temp_BM_A)
+	print("SPEED ",Temp_Sped)
--- lotth-rpg/scripts/resources/CH/Stat_increases_resource.gd
+++ lotth-rpg/scripts/resources/CH/Stat_increases_resource.gd
@@ -1,50 +1,45 @@
 extends Resource
 class_name Stat_Increases
 
-var Defense: int 
-var Magic_Defense: int 
-var Base_Phisical_Attack: int 
-var Base_Magical_Attack: int
-var Speed: int
 var header_ids: Array = []
+# Defense,Magic_Defense,Base_Phisical_Attack,Base_Magical_Attack,Speed
+var All_stats: Array
+
 # header for id stuff?
 # return [header,{stat1:value,stat2:value}, remaining]
 # store them inside a list [].
 
-func _Get_Current_Stats(Def,M_Def,BP_Attack,BM_Attack,Speed_turn,Headers) -> void:
-	self.Defense = Def
-	self.Magic_Defense = M_Def
-	self.Base_Phisical_Attack = BP_Attack
-	self.Base_Magical_Attack = BM_Attack
-	self.Speed = Speed_turn
+func _Get_Current_Stats(Headers) -> void:
 	self.header_ids = Headers
 
 func _Create_Header_Id():
 	var random_header_id = randi()
-	for i in header_ids:
-		if random_header_id == i:
-			_Create_Header_Id()
-		else: return random_header_id
+	
+	if random_header_id in header_ids:
+		_Create_Header_Id()
+	else: return random_header_id
 
 func _Stat_change(Limitation: String,Limitation_number: int,What_Stats: Dictionary) -> Array:
 	var random_header_id = _Create_Header_Id()
 	var is_turn: int = -1
-	var All_stats = [Defense,Magic_Defense,Base_Phisical_Attack,Base_Magical_Attack,Speed]
+	All_stats = [0,0,0,0,0]
 	match Limitation:
 		"Turn":
 			is_turn = Limitation_number
 		"Until": # later for event handler?
 			pass
-
+		"ROB": # Rest Of Battle
+			is_turn = -1
 	for i in What_Stats:
 		if What_Stats[i][2]:
-			_Buff(What_Stats[i][0])
+			_Buff(What_Stats[i])
+
 		else:
-			_Debuff(What_Stats[i][0])
+			_Debuff(What_Stats[i])
 	
 	if not is_turn <= 0:
 		return [random_header_id,All_stats,is_turn]
-	return [random_header_id,All_stats]
+	return [random_header_id,All_stats,is_turn]
 	
 # key 			value
 # index	[Stat,much,buff_debuff]
@@ -52,26 +47,26 @@ func _Stat_change(Limitation: String,Limitation_number: int,What_Stats: Dictiona
 func _Buff(What_Stats):
 	match What_Stats[0]:
 		"Def":
-			Defense += What_Stats[1]
+			All_stats[0] += What_Stats[1]
 		"M_Def":
-			Magic_Defense += What_Stats[1]
+			All_stats[1] += What_Stats[1]
 		"BP_Attack":
-			Base_Phisical_Attack += What_Stats[1]
+			All_stats[2] += What_Stats[1]
 		"BM_Attack":
-			Base_Magical_Attack += What_Stats[1]
+			All_stats[3] += What_Stats[1]
 		"Speed":
-			Speed += What_Stats[1]
+			All_stats[4] += What_Stats[1]
 
 	
 func _Debuff(What_Stats):
-	match What_Stats:
+	match What_Stats[0]:
 		"Def":
-			Defense -= What_Stats[1]
+			All_stats[0] -= What_Stats[1]
 		"M_Def":
-			Magic_Defense -= What_Stats[1]
+			All_stats[1] -= What_Stats[1]
 		"BP_Attack":
-			Base_Phisical_Attack -= What_Stats[1]
+			All_stats[2] -= What_Stats[1]
 		"BM_Attack":
-			Base_Magical_Attack -= What_Stats[1]
+			All_stats[3] -= What_Stats[1]
 		"Speed":
-			Speed -= What_Stats[1]
+			All_stats[4] -= What_Stats[1]
